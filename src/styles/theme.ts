206484e8f8dc2fd158fe03325da00d28f20eadff
Guilherme Amado <guiii@live.com>
2021-09-26 13:16:04 -0300

    configure fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  fonts: {
+    heading: 'Karla',
+    body: 'Karla',
+  },
   styles: {
     global: {
       body: {
