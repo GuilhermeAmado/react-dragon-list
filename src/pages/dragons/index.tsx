b2d6a0e6d93343e9ec2214c0588fa2147c2df027
Guilherme Amado <guiii@live.com>
2021-10-03 18:50:07 -0300

    fix dragons page

--- src/pages/dragons/index.tsx
+++ src/pages/dragons/index.tsx
@@ -1,18 +1,12 @@
 import { Container, Flex } from '@chakra-ui/react';
-import { useContext, useEffect } from 'react';
 import AddDragonButton from '../../components/AddDragonButton';
 import DragonList from '../../components/DragonList';
 import Logo from '../../components/Logo';
 import Router from 'next/router';
-import { AuthContext } from '../../contexts/AuthContext';
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
 
 const DragonsPage = () => {
-  const { user, isLoading } = useContext(AuthContext);
-
-  if (!isLoading && !user) return <h1>you seem lost friend...</h1>;
-
   return (
     <Container maxWidth="768px" mx="auto">
       <Flex
